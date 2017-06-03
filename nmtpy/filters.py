431c4ab5e9b2ab80d277dd42534be2f87440c381
Ozan Caglayan <ozancag@gmail.com>
2017-06-03 15:49:19 +0200

    filters: simply filters, add char2words

--- nmtpy/filters.py
+++ nmtpy/filters.py
@@ -1,53 +1,47 @@
 # -*- coding: utf-8 -*-
 import re
 
-class CompoundFilter(object):
-    """Filters out fillers from compound splitted sentences."""
-    def __init__(self):
-        pass
-
-    def __filter(self, s):
-        return s.replace(" @@ ", "").replace(" @@", "").replace(" @", "").replace("@ ", "")
-
+class Filter(object):
+    """Common Filter class for post-processing sentences."""
     def __call__(self, inp):
         if isinstance(inp, str):
-            return self.__filter(inp)
+            # Apply to single sentence
+            return self.process(inp)
         else:
-            return [self.__filter(e) for e in inp]
+            # Assume a sequence and apply to each
+            return [self.process(e) for e in inp]
 
-class BPEFilter(object):
+    def process(self, s):
+        # Derived classes should implement this method
+        return s
+
+class CompoundFilter(Filter):
+    """Filters out fillers from compound splitted sentences."""
+    def process(self, s):
+        return s.replace(" @@ ", "").replace(" @@", "").replace(" @", "").replace("@ ", "")
+
+class BPEFilter(Filter):
     """Filters out fillers from BPE applied sentences."""
-    def __init__(self):
-        pass
-
-    def __filter(self, s):
+    def process(self, s):
         # The first replace misses lines ending with @@
         # like 'foo@@ bar Hotel@@'
         return s.replace("@@ ", "").replace("@@", "")
 
-    def __call__(self, inp):
-        if isinstance(inp, str):
-            return self.__filter(inp)
-        else:
-            return [self.__filter(e) for e in inp]
-
-class DesegmentFilter(object):
+class DesegmentFilter(Filter):
     """Converts Turkish segmentations of <tag:morpheme> to normal form."""
-    def __init__(self):
-        pass
-
-    def __filter(self, s):
+    def process(self, s):
         return re.sub(' *<.*?:(.*?)>', '\\1', s)
 
-    def __call__(self, inp):
-        if isinstance(inp, str):
-            return self.__filter(inp)
-        else:
-            return [self.__filter(e) for e in inp]
+class Char2Words(Filter):
+    """Converts a space delimited character sequence to
+    normal word form. The output will be non-tokenized."""
+    def process(self, s):
+        return s.replace(' ', '').replace('<s>', ' ').strip()
 
 def get_filter(name):
     filters = {
                 "bpe"          : BPEFilter(),
+                "char2words"   : Char2Words(),
                 "compound"     : CompoundFilter(),
                 "desegment"    : DesegmentFilter(),
               }
